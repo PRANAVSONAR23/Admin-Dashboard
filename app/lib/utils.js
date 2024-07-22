516132cac054bebc1147866e58ca33ae99a16ee4
pranavsonar2311@gmail.com <114359363+PRANAVSONAR23@users.noreply.github.com>
2024-07-22 22:07:44 +0530

    first commit

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,14 +1,22 @@
-import mongoose from "mongoose";
+require('dotenv').config();
+import mongoose from 'mongoose';
 
 const connection = {};
 
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect(process.env.MONGO);
+    console.log('Mongo URI:', process.env.MONGO); // Check if this logs the correct URI
+    if (!process.env.MONGO) {
+      throw new Error('MONGO environment variable not defined');
+    }
+    const db = await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw new Error(error);
   }
 };
